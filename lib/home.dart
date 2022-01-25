37d134b36fa7deadb7de48460c5bc2c9addd9750
RoxyRoses <rosatuttie@gmail.com>
2022-01-25 14:37:42 -0300

    refatoracoes e melhorias

---
+++ lib/home.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'Metodo.dart';
+import 'package:flutter/services.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Teste 1',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const MyHomePage(title: 'Teste 1 - Jennifer Sarah'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final controladorTxtNumero = TextEditingController();
+  var resultado = "";
+  final calculo = Calcular();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Digite o numero: ',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              width: 200,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: controladorTxtNumero,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () async {
+                String retorno =
+                    await calculo.realizarCalculo(controladorTxtNumero.text);
+                setState(() {
+                  resultado = retorno;
+                });
+              },
+              child: const Text('Calcular'),
+            ),
+            Text(
+              resultado,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/services.dart';
 import 'Metodo.dart';
 import 'package:flutter/material.dart';
 
+import 'home.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -46,39 +48,105 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Digite o numero: ',
+              'Seleção de Desenvolvedor de Software - Escribo',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(
-              width: 200,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: controladorTxtNumero,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Teste Técnico 01',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Descrição',
+                    style: TextStyle(fontSize: 18),
+                  )),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Implemente uma função que receba um número inteiro positivo e retorne o somatório de todos os valores inteiros divisíveis por 3 ou 5 que sejam inferiores ao número passado',
+                    style: TextStyle(fontSize: 15),
+                  )),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Exemplos',
+                    style: TextStyle(fontSize: 18),
+                  )),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    ' Caso sua função receba o inteiro 10, ela deve retornar 23, resultante do somatório dos números 3, 5, 6 e 9 que são menores que 10. - Caso sua função receba o inteiro 11, ela deve retornar 33, resultante do somatório dos números 3, 5, 6, 9 e 10 que são menores que 11.',
+                    style: TextStyle(fontSize: 15),
+                  )),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Instruções',
+                    style: TextStyle(fontSize: 18),
+                  )),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Sua função deve ser implementada em Dart, PHP ou em Javascript. - Sua implementação deve estar em um repositório público no Github. - Escreva um README no seu repositório com as instruções de como rodar o seu projeto e como podemos testá-lo.- Envie sua resposta para o email jobs@escribo.com com o link do repositório.',
+                    style: TextStyle(fontSize: 15),
+                  )),
+            ),
+            MaterialButton(
+              child: const Text(
+                "Iniciar",
+                style: TextStyle(color: Colors.white),
               ),
-            ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () async {
-                String retorno =
-                    await calculo.realizarCalculo(controladorTxtNumero.text);
-                setState(() {
-                  resultado = retorno;
-                });
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Home()),
+                );
               },
-              child: const Text('Calcular'),
-            ),
-            Text(
-              resultado,
-              style: const TextStyle(fontWeight: FontWeight.bold),
+              color: Colors.red,
             )
           ],
         ),
